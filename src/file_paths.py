268c7f716fd32362867f3561f0a9996b795260e2
Leana Critchell <leana.m.critchell@gmail.com>
2020-08-10 23:38:07 -0700

    model iterations and viz

---
+++ src/file_paths.py
@@ -0,0 +1,14 @@
+# file paths to train/test/val directories with relative parent_dir 
+# be sure to make sure your parent_dir file path is correct for each new notebook 
+
+train_dir = parent_dir + 'data/seg_train/'
+test_dir = parent_dir + 'data/seg_test/'
+val_dir = parent_dir + 'data/seg_val/'
+
+# training class directories
+train_buildings_dir = parent_dir + 'data/seg_train/buildings'
+train_forest_dir = parent_dir + 'data/seg_train/forest'
+train_glacier_dir = parent_dir + 'data/seg_train/glacier'
+train_mountain_dir = parent_dir + 'data/seg_train/mountain'
+train_sea_dir = parent_dir + 'data/seg_train/sea'
+train_street_dir = parent_dir + 'data/seg_train/street'
