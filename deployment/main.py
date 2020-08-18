3912fc65bbee9fec3d9d4258a48336c535fc419a
Leana Critchell <leana.m.critchell@gmail.com>
2020-08-18 15:08:19 -0700

    app updates

--- deployment/main.py
+++ deployment/main.py
@@ -6,12 +6,17 @@ demo_images = os.listdir('static/images')
 
 app = Flask(__name__)
 
-@app.route("/home")
+@app.route("/home", methods=['GET'])
 @app.route("/")
 def home():
     images = list(map(lambda x: url_for('static', filename='images/' + x), demo_images))
     random.shuffle(images)
     return render_template("home.html", images=images)
+
+@app.route("/sort", methods=['POST'])
+def sort():
+    return render_template("sort.html")
+
 
 if __name__ == "__main__":
     app.run(debug = True)
