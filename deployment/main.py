322c3521d83303fe9ce7e5fc69ac153c10319ca7
Leana Critchell <leana.m.critchell@gmail.com>
2020-08-18 23:01:10 -0700

    app functionality

--- deployment/main.py
+++ deployment/main.py
@@ -2,20 +2,22 @@ from flask import Flask, render_template, url_for
 import os
 import random
 
-demo_images = os.listdir('static/images')
+demo_images = os.listdir('static/images/imgs')
+
+pred_dict = {'building': demo_images[:10], 'forest': demo_images[10:20], 'glacier': demo_images[20:]}
 
 app = Flask(__name__)
 
 @app.route("/home", methods=['GET'])
 @app.route("/")
 def home():
-    images = list(map(lambda x: url_for('static', filename='images/' + x), demo_images))
+    images = list(map(lambda x: url_for('static', filename='images/imgs' + x), demo_images))
     random.shuffle(images)
     return render_template("home.html", images=images)
 
 @app.route("/sort", methods=['POST'])
 def sort():
-    return render_template("sort.html")
+    return render_template("sort.html", image_preds = pred_dict)
 
 
 if __name__ == "__main__":
